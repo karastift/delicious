517ed36153964c4ace559c03a150b9383b97d44f
kara <kiansinan@gmail.com>
2021-07-24 21:17:09 +0200

    validateFood functions

---
+++ server/src/validations/validateFood.ts
@@ -0,0 +1,10 @@
+import { FieldError } from "../types/responses/errors/FieldError";
+import { FoodInput } from "../types/inputs/FoodInput";
+
+export const validateFood = (input: FoodInput): FieldError | undefined => {
+    if (input.foodName.length < 2) return {
+        field: 'foodName',
+        message: 'The name of you food has to have at least 2 characters.',
+    };
+    return undefined;
+};
