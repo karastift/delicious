c49c50ffa9d238ebb36f1cbf4f2a659d2ac72c74
kara <kiansinan@gmail.com>
2021-07-25 08:41:57 +0200

    changed house messages

--- server/src/messages/houseMessages.ts
+++ server/src/messages/houseMessages.ts
@@ -1,5 +1,5 @@
-export const houseNotFoundByName = 'There is no house with that name.';
-export const houseNotFoundById = 'House does not exist.';
+export const houseNotFoundByName = 'The house with that name is uninhabited.';
+export const houseNotFoundById = 'House is uninhabited.';
 export const houseNameDuplicate = 'The already is another house with with that name.';
-export const houseToUpdateNotFound = 'Your house does not exist for some reason. Maybe you deleted it.';
+export const houseToUpdateNotFound = 'Your house does not exist for some reason. Maybe you moved out.';
 export const incorrectPassword = 'Your password is not correct.';
