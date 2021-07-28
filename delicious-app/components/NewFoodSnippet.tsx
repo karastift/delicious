bd5b2a35f1faf8e1f8d247635a298345d24bc969
kara <kiansinan@gmail.com>
2021-07-28 12:27:15 +0200

    changed all snippet designs

--- delicious-app/components/NewFoodSnippet.tsx
+++ delicious-app/components/NewFoodSnippet.tsx
@@ -1,35 +1,26 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, Linking } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
 import { colors } from '../themes/Lighttheme';
 import { beautifyDate } from '../utils/beautifyDate';
 import { beautifyTime } from '../utils/beautifyTime';
 
-export const NewWishSnippet = (props: {
+export const NewFoodSnippet = (props: {
   foodName: string;
-  time: string;
-  pending?: boolean;
-  suggestingMember: string;
-  assignedMember: string;
+  description?: string;
+  creator: string;
+  recipeLink?: string;
   onPress: () => void;
 }) => {
 
   return (
     <TouchableOpacity style={styles.memberBlockWrapper}>
-      {props.pending
-      ? <FontAwesomeIcon name='hourglass-end' size={50} style={styles.icon}/>
-      : <FontAwesomeIcon name='hourglass-start' size={50} style={styles.icon}/>
-      }
-      {/* <View style={styles.categoryWrapper}>
-        <Text style={styles.categoryText}>name:</Text>
-        <Text style={styles.categoryText}>role:</Text>
-        <Text style={styles.categoryText}>food made:</Text>
-      </View> */}
+      <FontAwesomeIcon name='apple-alt' size={50} style={styles.icon}/>
       <View style={styles.infoWrapper}>
         <Text style={styles.highlightedText}>{props.foodName}</Text>
-        <Text style={styles.text}>{props.pending ? beautifyTime(props.time) : beautifyDate(props.time)}</Text>
-        <Text style={styles.text}><Text style={styles.highlightedText}>{props.suggestingMember}</Text> suggested this,</Text>
-        <Text style={styles.text}>assigned to <Text style={styles.highlightedText}>{props.suggestingMember}</Text></Text>
+        <Text style={styles.text}>{props.description}</Text>
+        <Text style={styles.text}>created by <Text style={styles.highlightedText}>{props.creator}</Text></Text>
+        {props.recipeLink ? <Text style={styles.text} onPress={() => Linking.openURL(props.recipeLink!)}><Text style={styles.highlightedText}>{props.recipeLink}</Text></Text> : <></>}
       </View>
     </TouchableOpacity>
   );
