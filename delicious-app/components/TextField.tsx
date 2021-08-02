701a77c9bb3f68519c4b4c9dd381f52979d2185b
kara <kiansinan@gmail.com>
2021-08-02 10:51:46 +0200

    login screen

--- delicious-app/components/TextField.tsx
+++ delicious-app/components/TextField.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ViewStyle } from "react-native";
 import { StyleSheet } from "react-native";
 import { TextInput } from "react-native-paper";
 import { colors } from "../themes/Lighttheme";
@@ -7,25 +8,26 @@ export const TextField = (props: {
   label: string;
   error: boolean;
   onChangeText: (arg0: string) => void;
-  passwordRules?: string;
+  password?: boolean;
   autoFocus?: boolean;
+  iconName: string;
+  style?: ViewStyle;
 }) => {
 
   return (
-    <>
-      <TextInput
-        label={props.label}
-        error={props.error}
-        onChangeText={props.onChangeText}
-        passwordRules={props.passwordRules}
-        autoFocus={props.autoFocus}
-        underlineColor={colors.highlighted}
-        underlineColorAndroid={colors.highlighted}
-        outlineColor={colors.highlighted}
-        selectionColor={colors.text}
-        style={styles.textInput}
-      />
-    </>
+    <TextInput
+      mode='outlined'
+      label={props.label}
+      error={props.error}
+      onChangeText={props.onChangeText}
+      autoFocus={props.autoFocus}
+      outlineColor={colors.darkHiglighted}
+      selectionColor={colors.lighterText}
+      style={[styles.textInput, props.style]}
+      theme={{ colors: { primary: colors.darkHiglighted } }}
+      secureTextEntry={props.password}
+      left={<TextInput.Icon name={props.iconName} style={{ marginTop: 15, marginRight: 10 }} color={colors.lighterText}/>}
+    />
   );
 };
 
@@ -35,6 +37,6 @@ const styles = StyleSheet.create({
     height: 50,
     paddingHorizontal: 10,
     fontSize: 18,
-    color: colors.highlighted
+    color: colors.highlighted,
   },
 });
